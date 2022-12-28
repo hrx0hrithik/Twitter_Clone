7e0b1affa15948bd0307104376aed202a5ee5015
hrx0hrithik <hrithikgupta52@gmail.com>
2022-12-29 04:14:22 +0530

    backend

--- index.js
+++ index.js
@@ -1,25 +1,73 @@
-const loginForm = document.getElementById('login-form');
-    loginForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const username = document.getElementById('login-username').value;
-      const password = document.getElementById('login-password').value;
-      if (!username || !password) {
-        alert('Please enter a username and password');
-      } else {
-        loginForm.submit();
-      }
-    });
 
-    // Validate signup form input
-    const signupForm = document.getElementById('signup-form');
-    signupForm.addEventListener('submit', (event) => {
-      event.preventDefault();
-      const username = document.getElementById('signup-username').value;
-      const password = document.getElementById('signup-password').value;
-      const email = document.getElementById('signup-email').value;
-      if (!username || !password || !email) {
-        alert('Please enter a username, password, and email');
-      } else {
-        signupForm.submit();
-      }
+
+function showLoginForm() {
+    document.getElementById("login-form").style.display = "block";
+    document.getElementById("signup-form").style.display = "none";
+  }
+  function showSignupForm() {
+    document.getElementById("login-form").style.display = "none";
+    document.getElementById("signup-form").style.display = "block";
+  }
+// Initialize Firebase (ADD YOUR OWN DATA)
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA9tqWOfst1p6qJnrpA7M0O_AkVc1pzBsA",
+    authDomain: "tweeter-clone-713cc.firebaseapp.com",
+    databaseURL: "https://tweeter-clone-713cc-default-rtdb.firebaseio.com",
+    projectId: "tweeter-clone-713cc",
+    storageBucket: "tweeter-clone-713cc.appspot.com",
+    messagingSenderId: "646741689671",
+    appId: "1:646741689671:web:c05072f983366535ee3862",
+    measurementId: "G-H5G9B3NLXM"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  
+  // Reference messages collection
+  var messagesRef = firebase.database().ref('messages');
+  
+  // Listen for form submit
+  document.getElementById('signup-form').addEventListener('submit', submitForm);
+  
+  // Submit form
+  function submitForm(e){
+    e.preventDefault();
+  
+    // Get values
+    var name = getInputVal('name');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var dob = getInputVal('dateOfBirth');
+    var pass = getInputVal('password')
+  
+    // Save message
+    saveMessage(name, email, phone, dob, pass);
+  
+    // Show alert
+    document.querySelector('.alert').style.display = 'block';
+  
+    // Hide alert after 3 seconds
+    setTimeout(function(){
+      document.querySelector('.alert').style.display = 'none';
+    },3000);
+  
+    // Clear form
+    document.getElementById('signup-form').reset();
+  }
+  
+  // Function to get get form values
+  function getInputVal(id){
+    return document.getElementById(id).value;
+  }
+  
+  // Save message to firebase
+  function saveMessage(name, email, phone, dob, pass){
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+      name: name,
+      email:email,
+      phone:phone,
+      dob: dob,
+      pass: pass,
     });
+  }
