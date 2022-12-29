f051fa22f92fefdc1bfe1bdc9461f3e6f90c74c4
hrx0hrithik <hrithikgupta52@gmail.com>
2022-12-29 21:35:55 +0530

    changes

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const moreBtn = document.querySelector('#moreBtn');
     const subItemsBox = document.querySelector('.sub-items-box');
+    const subMenuBox = document.querySelector('.sub-menu');
     const overlay = document.querySelector('.overlay')
+    const profile_menu = document.querySelector('#profile_info')
 
     moreBtn.addEventListener('click', function () {
       subItemsBox.classList.add('active');
@@ -9,5 +11,11 @@ const moreBtn = document.querySelector('#moreBtn');
 
     overlay.addEventListener('click', function () {
       subItemsBox.classList.remove('active');
+      subMenuBox.classList.remove('active');
       overlay.classList.remove('active');
+    })
+
+    profile_menu.addEventListener('click', function () {
+      subMenuBox.classList.add('active');
+      overlay.classList.add('active');
     })
