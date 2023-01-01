5e76efe7608e6e2bec68fd4e810513c327d01013
hrx0hrithik <hrithikgupta52@gmail.com>
2023-01-02 02:16:36 +0530

    password updated

--- index.js
+++ index.js
@@ -2,8 +2,26 @@ const secondPart = document.querySelector('.part-two');
 const firstPart = document.querySelector('.part-one');
 const nextBtn = document.querySelector('.next-btn');
 
-nextBtn.addEventListener('click', function(){
+const showPassword = document.querySelector('#show-password');
+const passwordField = document.querySelector('#password');
+
+
+nextBtn.addEventListener('click', function () {
     firstPart.classList.add('active');
     secondPart.classList.add('active');
     console.log('clicked');
 });
+
+var clicked = false;
+function toogle() {
+    if (!clicked) {
+        clicked = true;
+        showPassword.innerHTML = 'visibility_off';
+        passwordField.setAttribute("type", "text");
+    } else {
+        clicked = false;
+        showPassword.innerHTML = 'visibility';
+        passwordField.setAttribute("type", "password");
+    }
+}
+
