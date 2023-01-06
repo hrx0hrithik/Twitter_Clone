9e51d42ff7ebf4187ce6adaf8e66ab78d67fac4a
hrx0hrithik <hrithikgupta52@gmail.com>
2023-01-06 20:05:38 +0530

    .

--- main.js
+++ main.js
@@ -1,3 +1,37 @@
+
+const profileBtn = document.querySelector('.profile-box');
+const moreBtn = document.querySelector('#more-btn');
+const profileMenu = document.querySelector('.profile-menu');
+const moreMenu = document.querySelector('.more-menu');
+
+
+const creatorBtn = document.querySelector('.creator-btn');
+const showCreator = document.querySelector('.creator-menu');
+const proBtn = document.querySelector('.pro-btn');
+const showPro = document.querySelector('.pro-menu');
+const settingBtn = document.querySelector('.setting-btn');
+const showSetting = document.querySelector('.setting-menu');
+
+proBtn.addEventListener("click", function(){
+    showPro.classList.toggle("show");
+})
+settingBtn.addEventListener("click", function(){
+    showSetting.classList.toggle("show");
+})
+creatorBtn.addEventListener("click", function(){
+    showCreator.classList.toggle("show"); 
+})
+
+
+
+profileBtn.addEventListener("click", function() {
+    profileMenu.classList.toggle("activated");
+});
+moreBtn.addEventListener("click", function (){
+    moreMenu.classList.toggle("activated");
+})
+
+
 // document.addEventListener('click', e => {
 //     const isSubmenuButton = e.target.matches("[data-submenu-button]")
 //     if (!isSubmenuButton && e.target.closest('[data-submenu]') != null) return
@@ -13,16 +47,3 @@
 //         submenu.classList.remove('active')
 //     })
 // })
-
-const profileBtn = document.querySelector('.profile-box');
-const moreBtn = document.querySelector('#more-btn');
-const profileMenu = document.querySelector('.profile-menu');
-const moreMenu = document.querySelector('.more-menu')
-profileBtn.addEventListener("click", function() {
-    profileMenu.classList.toggle("activated");
-    // console.log("clicked");
-});
-moreBtn.addEventListener("click", function (){
-    moreMenu.classList.toggle("activated");
-     console.log("clicked");
-})
